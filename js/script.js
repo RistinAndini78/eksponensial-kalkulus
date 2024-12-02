0d693d296ab6b8ca04a5c1abc09516bf183a3195
RistinAndini78 <ristinristin798@gmail.com>
2024-12-02 12:01:14 +0700

    pesan

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
+// Menunggu hingga seluruh konten halaman selesai dimuat
 document.addEventListener("DOMContentLoaded", function () {
+    // Mendapatkan elemen navbar
     const navbar = document.querySelector(".navbar");
 
+    // Menambahkan event listener untuk mendeteksi pergerakan scroll
     window.addEventListener("scroll", () => {
+        // Jika scroll melewati 100px, tambahkan efek blur pada navbar
         if (window.scrollY > 100) {
             navbar.classList.add("blur-background");
         } else {
+            // Jika kurang dari 100px, hapus efek blur
             navbar.classList.remove("blur-background");
         }
     });
 });
 
+// Fungsi untuk menampilkan atau menyembunyikan menu
 function toggleMenu() {
+    // Mendapatkan elemen menu
     const menu = document.querySelector('.menu');
+    // Menambahkan atau menghapus kelas "open" untuk mengatur tampilan menu
     menu.classList.toggle('open');
 }
-
-
-
-
